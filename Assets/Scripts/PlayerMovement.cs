fe369637a3950041de28d982ac015862e20dc14e
Xpyto <69775025+Xpyto@users.noreply.github.com>
2021-04-27 17:08:38 +1000

    basicMovement
    basicMovement done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,40 +4,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public bool isGrounded;
+    float moveSpeed;
+    float jumpMultiplier;
+    float jumpVelocity;
+    float fallMultiplier;
+    bool isGrounded;
+    public bool isMoving;
 
-    // Start is called before the first frame update
+    float distToGround;
+
+    Rigidbody2D rb;
+    Collider2D cd;
+    
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        cd = GetComponent<Collider2D>();
+    }
+
     void Start()
     {
+        moveSpeed = 5f;
+        jumpMultiplier = 2f;
+        jumpVelocity = 6f;
+        fallMultiplier = 2.5f;
+        isGrounded = true;
+        isMoving = false;
+        distToGround = cd.bounds.extents.y;
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Jump") && isGrounded){
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal"))==1){
+            transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed, 0f, 0f);
+        }
+        
+        //Debug.Log(isGrounded);
+
+        if(rb.velocity.y < 0){
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }else if(rb.velocity.y > 0 && !Input.GetButton("Jump")){
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (jumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        
+    }
+
+    
+    private void OnCollisionStay2D(Collision2D other) {
+        CheckIfGrounded();
+    }
+ 
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Floor")){
+        isGrounded = false;
+        }
+    }
+ 
+    
+    /*void IsGrounded(){
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }*/
+
+    private void CheckIfGrounded()
+{
+    RaycastHit2D[] hits;
+
+    //We raycast down 1 pixel from this position to check for a collider
+    Vector2 positionToCheck = transform.position;
+    hits = Physics2D.RaycastAll (positionToCheck, new Vector2 (0, -1), 0.05f);
+
+    //if a collider was hit, we are grounded
+    if (hits.Length > 0) {  
         isGrounded = true;
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        /*if(Input.GetButton("up") && isGrounded){
-            transform.position += new Vector3(0f, moveSpeed, 0f);
-        }*/
-        if(Input.GetButton("left")){
-            transform.position += new Vector3(moveSpeed, 0f, 0f);
-        }   
-        if(Input.GetButton("right")){
-            transform.position += new Vector3(-1*moveSpeed,0f, 0f);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collider){
-        Invoke("NoGround",3f);
-    }
-
-    private void OnCollisionStay2D(Collision2D collider){
-        if(collider.gameObject.CompareTag("Floor")){
-            isGrounded = true;
-        }
-    }
-
-    void NoGround(){
-        isGrounded = false;
-    }
+}
 }
