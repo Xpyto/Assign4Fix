3ae95b1e77b9ae106ce29abed345e59fd0849b0b
Xpyto <0rickcir0@gmail.com>
2021-05-13 00:01:22 +1000

    Successful merge

--- Assets/Scripts/Core/PlayerMovement.cs
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -73,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
         }
         }
         
-        if(transform.position.y < -6){
+        if(transform.position.y < -50){
             isDead = true;
         }
     }
